c7b2acb1cd1ce02d18e957fe8e85a88c2d034ecd
Nagendra Dubey <nagendrathegreat1998@gmail.com>
2023-02-16 12:05:01 +0530

    third commit

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,26 @@
+const Post=require('../models/post');
+
 module.exports.home = function(req,res){
     //return res.end('<h1>Express is up for codeial!</h1> ');
-    console.log(req.cookies);
-    return res.render('home',{
-    title:"Home"
-});
+    //console.log(req.cookies);
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
 
+    //         title:"Codeial | Home",
+    //         posts:posts
+    //     });
+        
+    // });
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+
+            title:"Codeial | Home",
+            posts:posts
+        });
+        
+    }
+
+    )
 }
 
 //module.exports.actioName=function(req,res){}
